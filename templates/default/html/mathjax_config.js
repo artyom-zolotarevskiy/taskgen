15c0be41f3335a3efbdeb3e61f462f1eb0f5a536
Artyom Zolotarevskiy <metrazlot@gmail.com>
2023-03-12 23:11:44 +0300

    removed unnecessery logs

--- templates/default/html/mathjax_config.js
+++ templates/default/html/mathjax_config.js
@@ -4,30 +4,19 @@
     console.log('Starting Table scaling...');
     // максимальная ширина таблицы в мм
     const max_table_width = document.body.offsetWidth / scrinPPMM - body_padding_mm * 2;
-    //document.getElementsByTagName('body')[0].innerHTML = 'max_table_width=' + max_table_width;
-    //document.body.innerHTML = 'body_padding=' +
-    //    window.getComputedStyle(document.body, null).getPropertyValue('padding-left');
-    //document.body.innerHTML = 'Кол-во пикселей в одном миллиметре на данном экране: ' +
-    //  scrinPPMM;
     // находим все таблицы
     const tables = document.getElementsByTagName('mjx-mtable');
     for (const table of tables) {
       let table_width_mm = table.offsetWidth / scrinPPMM;
       if (table_width_mm > max_table_width) {
-        console.log(table);
-        console.log('Таблица превышает допустимую ширину: ' +
-          table_width_mm + ' мм');
-        console.log('Рассчитываем коэффициент масштабирования...');
         let scale_factor = 1 / (table_width_mm / max_table_width);
-        console.log('Коэффициент масштабирования: ', scale_factor)
         table.style.transform = 'scale(' + scale_factor + ')';
-        console.log('Применили масштабирование');
         table.style.marginLeft = '-' +
           (table.offsetWidth * (1 - scale_factor) / 2 + 15) + 'px';
-        console.log('Выровнили таблицы по центру');
       }
     }
     window.mathjax_loaded = true;
+    console.log('Scaling of tables is completed!');
   }
   window.mathjax_loaded = false;
   window.MathJax = {
@@ -49,7 +38,7 @@
           // кол-во пикселей в одном миллиметре на данном экране
           screenPPI = document.getElementById('ppitest').offsetWidth;
           scrinPPMM = screenPPI / 25.4;
-          console.log('Кол-во пикселей в одном миллиметре на данном экране: ' +
+          console.log('Number of pixels in one millimeter on this screen: ' +
             scrinPPMM)
           body_padding_mm = window.getComputedStyle(document.body, null).
           getPropertyValue('padding-left').replace('px', '') / scrinPPMM;
